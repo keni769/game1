925cdf9b630ae6d1f55b2b9da4e51ec84426bd2e
Sumit <abcddab1@gmail.com>
2026-09-16 21:16:08 +0530

    2

--- game/Global.gd
+++ game/Global.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 var minigames_done = 0 #track how many minigames done
 var lives = 5 #track how many lives left, also effecting garlic appearing
